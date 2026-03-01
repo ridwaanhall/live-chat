bcd7ceda09e08eef57a7e389ae9fdb61942c7f62
Ridwan Halim <hi@ridwaanhall.com>
2026-03-01 22:05:39 +0700

    Add support for strict password validation based on environment variable

--- gglinks/settings.py
+++ gglinks/settings.py
@@ -32,6 +32,9 @@ DEBUG = env("DEBUG", default=False, cast=bool)
 # Sign in with Google (set to True to use Google OAuth, False for Django auth)
 SIGNIN_GOOGLE = env("SIGNIN_GOOGLE", default=False, cast=bool)
 
+# Use strict password validation (set to True to enforce strong passwords)
+USE_HARD_PASSWORD = env("USE_HARD_PASSWORD", default=False, cast=bool)
+
 ALLOWED_HOSTS = []
 
 
@@ -121,20 +124,23 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+if USE_HARD_PASSWORD:
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
+else:
+    AUTH_PASSWORD_VALIDATORS = []
 
 
 # Internationalization
